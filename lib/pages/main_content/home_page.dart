86fd5d0baf4002e898ed60963c172a1d8ce70ecd
Ridhiwan <ridhiwanmseya@gmail.com>
2023-08-11 13:47:27 +0300

    search results suggestions display framework completed

--- lib/pages/main_content/home_page.dart
+++ lib/pages/main_content/home_page.dart
@@ -10,6 +10,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ScrollController scrollController = ScrollController();
   final List<String> imgList = [
     'https://images.unsplash.com/photo-1691071716244-db306a482fc0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
     'https://images.unsplash.com/photo-1691264122434-3b5a1dac81d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=928&q=80',
@@ -95,146 +96,172 @@ class _HomePageState extends State<HomePage> {
         shadowColor: const Color.fromRGBO(50, 94, 135, 1),
         backgroundColor: const Color.fromRGBO(0, 101, 168, 1),
       ),
-      body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) {
-          return [];
-        },
-        body: Stack(
+      body: RawScrollbar(
+        controller: scrollController,
+        thumbColor: const Color.fromRGBO(196, 102, 12, 0.6),
+        thickness: 8,
+        child: ListView(
+          controller: scrollController,
           children: [
-            Positioned(
-                top: 0,
-                bottom: screenHeight/3,
-                left: 0,
-                right: 0,
-                child: CarouselSlider(
-                  options: CarouselOptions(
-                    autoPlay: true,
-                    clipBehavior: Clip.hardEdge,
-                    autoPlayInterval: const Duration(seconds:5),
+            SizedBox(
+              height: (screenHeight*2)/4,
+              child: Stack(
+                children: [
+                  Positioned(
+                      top: 0,
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: CarouselSlider(
+                        options: CarouselOptions(
+                          autoPlay: true,
+                          clipBehavior: Clip.hardEdge,
+                          autoPlayInterval: const Duration(seconds:5),
+                        ),
+                        items: imageSliders,
+                      )
                   ),
-                  items: imageSliders,
-                )
+                  Positioned(
+                      top: screenHeight/13,
+                      left: screenWidth/2.6,
+                      child: Text('Tanzania Accessible Data',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: screenWidth/45,
+                          fontWeight: FontWeight.w700,
+                        ),)
+                  ),
+                  Positioned(
+                      top: screenHeight/5,
+                      left: screenWidth/3,
+                      child: Text('"We believe in the power of data custodianship."',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: screenWidth/60,
+                          fontWeight: FontWeight.w700,
+                        ),)
+                  ),
+                  Positioned(
+                      top: screenHeight/3,
+                      left: screenWidth/4.4,
+                      child: SearchBar(
+                        controller: searchController,
+                        hintText: 'Search datasets',
+                        backgroundColor: searchBarColor,
+                        constraints: BoxConstraints(maxWidth: screenWidth/1.85),
+                        side: MaterialStateProperty.all( const BorderSide(
+                          style: BorderStyle.solid,
+                          color: Color.fromRGBO(196, 102, 12, 1),
+                        )),
+                        elevation: MaterialStateProperty.all(3.0),
+                        trailing: [
+                          Center(
+                            child: IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.search_rounded,
+                                  color: const Color.fromRGBO(196, 102, 12, 1),
+                                  size: screenWidth/45,)),
+                          ),
+                        ],
+                        onChanged: (query) async{
+                          await fetchSuggestions(query);
+                        },
+                      )
+                  ),
+                ],
+              ),
             ),
-            Positioned(
-                top: screenHeight/13,
-                left: screenWidth/2.6,
-                child: Text('Tanzania Accessible Data',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: screenWidth/45,
-                  fontWeight: FontWeight.w700,
-                ),)
+            SizedBox(
+              height: screenHeight/40,
             ),
-            Positioned(
-                top: screenHeight/5,
-                left: screenWidth/3,
-                child: Text('"We believe in the power of data custodianship."',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: screenWidth/60,
-                  fontWeight: FontWeight.w700,
-                ),)
-            ),
-            Positioned(
-                top: screenHeight/3,
-                left: screenWidth/4.4,
-                child: SearchBar(
-                  controller: searchController,
-                  hintText: 'Search datasets',
-                  backgroundColor: searchBarColor,
-                  constraints: BoxConstraints(maxWidth: screenWidth/1.85),
-                  side: MaterialStateProperty.all( const BorderSide(
-                    style: BorderStyle.solid,
-                    color: Color.fromRGBO(196, 102, 12, 1),
-                  )),
-                  elevation: MaterialStateProperty.all(3.0),
-                  trailing: [
-                    Center(
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.search_rounded,
-                            color: const Color.fromRGBO(196, 102, 12, 1),
-                            size: screenWidth/45,)),
-                    ),
-                  ],
-                  onChanged: (query) async{
-                    await fetchSuggestions(query);
-                  },
-                )
-            ),
-            Positioned(
-              top: screenHeight/1.58,
-              left: screenWidth/4,
-              child: SizedBox(
-                height: screenHeight/4,
-                width: screenWidth/2,
-                child: ListView.builder(
-                  itemCount: suggestions.length,
-                  itemBuilder: (context, index) {
-                      return ListTile(
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: suggestions.length,
+              itemBuilder: (context, index) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: screenWidth/1.6,
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color.fromRGBO(196, 102, 12, 0.7),
+                            style: BorderStyle.solid,
+                            width: 2,
+                          ),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0,1),
+                            color: Color.fromRGBO(196, 102, 12, 0.7),
+                            blurRadius: 2,
+                            blurStyle: BlurStyle.outer,
+                          )
+                        ]
+                      ),
+                      child: ListTile(
                         title: Text(suggestions[index]['title']),
                         subtitle: Text(suggestions[index]["added"]),
                         textColor: Colors.black,
-                        tileColor: const Color.fromRGBO(0, 101, 168, 0.4),
-                      );
-                    },
-                ),
-              ),
+                        tileColor: Colors.white54,
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
-            Positioned(
-              top: screenHeight / 1.58,
-              left: screenWidth / 2.6,
-              child: InkWell(
-                onTap: () {},
-                child: Container(
-                  height: screenHeight / 5,
-                  width: screenWidth / 4,
-                  decoration: BoxDecoration(
+            SizedBox(height: screenHeight/10,),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                height: screenHeight / 5,
+                width: screenWidth / 4,
+                decoration: BoxDecoration(
                     color: const Color.fromRGBO(0, 101, 168, 1),
                     borderRadius: BorderRadius.circular(60),
                     boxShadow: const [
                       BoxShadow(
-                      color: Color.fromRGBO(196, 102, 12, 0.8),
+                        color: Color.fromRGBO(196, 102, 12, 0.8),
                         offset: Offset(0,3),
                         blurRadius: 6,
                       )
                     ]
-                  ),
-                  constraints: BoxConstraints(
-                    maxHeight: screenHeight / 5,
-                    maxWidth: screenWidth / 4,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Upload Dataset',
+                ),
+                constraints: BoxConstraints(
+                  maxHeight: screenHeight / 5,
+                  maxWidth: screenWidth / 4,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Upload Dataset',
                           style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.w700,
                             fontSize: screenWidth/50,
                           ),),
-                          SizedBox(width: screenWidth/50,),
-                          Icon(Icons.upload_file_outlined,
+                        SizedBox(width: screenWidth/50,),
+                        Icon(Icons.upload_file_outlined,
                           color: const Color.fromRGBO(196, 102, 12, 1),
-                            size: screenWidth/30,
-                          )
-                        ],
-                      ),
-                      SizedBox(height: screenHeight/50,),
-                      Text('file types: .csv, .xlsx, .shp, .odt',
+                          size: screenWidth/30,
+                        )
+                      ],
+                    ),
+                    SizedBox(height: screenHeight/50,),
+                    Text('file types: .csv, .xlsx, .shp, .odt',
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: screenWidth/80,
-                        fontWeight: FontWeight.w500
+                          color: Colors.white,
+                          fontSize: screenWidth/80,
+                          fontWeight: FontWeight.w500
                       ),)
-                    ],
-                  ),
+                  ],
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
